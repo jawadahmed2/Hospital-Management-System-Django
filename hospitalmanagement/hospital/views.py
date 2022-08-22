185d2914a2c9d66fee61d1de85c48fbf3b99c1ca
Jawad Ahmed <96545047+jawadahmed2@users.noreply.github.com>
2022-08-22 11:29:38 +0500

    index.html update

--- hospitalmanagement/hospital/views.py
+++ hospitalmanagement/hospital/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,reverse
+from django.http import HttpResponseRedirect
 
 # Create your views here.
+
+# View for home page
+def home_view(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('afterlogin')
+    return render(request,'hospital/index.html')
